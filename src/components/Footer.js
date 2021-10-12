23ad5454ee360a6a9dfd0cb20642fd1d57040290
iCode <gbengaobafemi1@gmail.com>
2021-10-12 10:19:35 +0100

    changed btn

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,12 @@
 import React from "react";
 import styles from "./styles.module.css";
-
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <a
-        href="mailTo:businesssupport@lotusbetaanalytics.com"
-        className={styles.btn}
-      >
-        Let's get Started
-      </a>
+      <Link to="/register" className={styles.btn}>
+        Register
+      </Link>
       <br />
       <br />
       <br />
